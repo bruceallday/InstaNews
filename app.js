71fe6282ba72cf4da178d4d600972b49c63cd986
Bruce Pouncey <brucepouncey@Bruces-MacBook-Pro.local>
2019-10-24 00:41:47 -0400

    feat: Added JSON API

---
+++ app.js
@@ -0,0 +1,24 @@
+$(function(){
+    $(function(){
+        $("select").on("change", function(event){
+            $selection = $(event.target).val()
+            // console.log($selection);
+            $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=WU7IFUdwRwP8g4fBGiav2zqEpSm6VlfG`)
+            .done(function(data){
+                // console.log(data)
+                $("section").remove();
+                $.each(data.results, function(key, value){
+                    console.log(key)
+                    console.log(value)
+                    $("main").append(`
+                    <section style="background-image: url(${value.multimedia[0].url});">
+                    <div class="abstract">
+                        <p>${value.abstract}</p>
+                    </div>
+                        
+                    </section>`)
+                })
+            })
+        })
+    })
+})
