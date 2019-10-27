39f0b85e12bdc5bc4dfd3f723826f66ff9209df5
Bruce Pouncey <brucepouncey@Bruces-MacBook-Pro.local>
2019-10-27 16:00:00 -0400

    style: finished abstract transitions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(function(){
     $(function(){
-
+        
         $("select").on("change", function(event){
             $("section").fadeOut(400);
             $(".loader").removeClass("hidden")
@@ -9,22 +9,22 @@ $(function(){
                 $(".logo").addClass(" logoShrinkMobile")
                 $("header").addClass(" headerShrinkMobile")
 
-            }else if($(window).width() > 768 ){
+            }else if($(window).width() > 767 ){
                 $(".logo").addClass(" logoShrinkTa")
                 $("header").addClass(" headerShrinkTa")
             }
+
             $selection = $(event.target).val()
 
             $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=WU7IFUdwRwP8g4fBGiav2zqEpSm6VlfG`)
             .done(function(data){
                 console.log(data)
-                let arrLength = 0;
+                let $arrLength = 0;
                 $.each(data.results, function(key, value){
-                    if(value.multimedia.length !==0 && arrLength < 12){
-                        arrLength ++;
+                    if(value.multimedia.length !==0 && $arrLength < 12){
+                        $arrLength += 1;
                         console.log(key)
                         console.log(value)
-
                         const $section = (`
                                             <section class="newsSection" style="background-image: url(${value.multimedia[4].url});">
                                                 <div class="abstract">
@@ -32,30 +32,24 @@ $(function(){
                                                 </div>
                                             </section>
                         `)
-                        $(".abstract").slideDown(100);
+
                         $("main").append($section).hide().fadeIn(700)
                         $(".loader").addClass(" hidden")
-                        
                     }
-
                 })
-                
+
                 $("section").mouseover(function(){
-                    $(this).children().slideUp("slow")
+                    $(this).children().slideDown("fast")
                 })
-                $("section").mouseout(function () {
-                    $(this).children().slideDown("slow")
+                $("section").mouseout(function (){
+                    $(this).children().slideUp("fast")
                 })
-
 
             })
-            
             
         })
 
     })
-
-    
 
 })
 
